ad18c1507e05c73053fcd141a9d2adbf743025b6
Shuncio <pplonka@protonmail.ch>
2022-06-12 03:48:12 +0200

    Deleted unnecessary parts, enhanced the correctness

--- js/index.js
+++ js/index.js
@@ -1,86 +1,91 @@
-    // Detecting mouse clicks on the drum buttons via the for loop
+    // detects mouse clicks on the drum btns via the for loop
 const totalAmountOfSoundableButtons = document.querySelectorAll(".drum-button").length;
 
 for(i = 0; i < totalAmountOfSoundableButtons; i++) {
-
-        // depending on pressed button, loop is assigning different number to it and immediately runs the function code below
+        // depending on the pressed button, the loop is assigning different number to it and immediately runs the function code below
     document.querySelectorAll(".drum-button")[i].addEventListener("click", function(event) {
         let actuallyClickedButton = this.innerHTML;
 
-            // calling the function that outputs the proper sound; giving the input to it of the exact char assigned to the actually clicked drum button
+            // calss the function that outputs the proper sound; giving the input to it of the exact char assigned to the actually clicked drum btn
         makeProperSound(actuallyClickedButton);
     
-            // calling the function that plays the animation after mouse click; giving it the input of the exact char assigned to the actually clicked drum button
+            // calss the function that plays the animation after mouse click; giving it the input of the exact char assigned to the actually clicked drum btn
         drumButtonAnimation(actuallyClickedButton);
     });
-
 }
 
-    // Detecting keyboard clicks on the drum buttons
-document.addEventListener("keydown", function(event) {
-        
-        // calling the function that gives off the sounds; giving the input to it of actually pressed keyboard key
+    // detects keyboard clicks on the drum buttons
+document.addEventListener('keydown', function(event) {
+    // calls the function that gives off the sounds; giving the input to it of actually pressed keyboard key
     makeProperSound(event.key); 
 
-        // calling the function that plays the animation after keyboard key press; giving the input to it of actually pressed keyboard key
+    // calls the function that plays the animation after keyboard key press; giving the input to it of actually pressed keyboard key
     drumButtonAnimation(event.key);
 });
 
-
-    // Downloading the given chars from 'click' and 'keydown' events
-function makeProperSound(key) {
-
-        // selecting the exact case based on the given char & giving giving off the proper sound
+// dwnlds the given chars from 'click' and 'keydown' events
+let makeProperSound = (key) => {
+    // selects the exact case based on the given char & giving giving off the proper sound
     switch (key) {
-        case "q": 
-            let tom1Sound = new Audio("sounds/tom-1.mp3");
+        case 'q': 
+            let tom1Sound = new Audio('sounds/tom-1.mp3');
             tom1Sound.play();
         break;
 
-        case "w":
-            let tom2Sound = new Audio("sounds/tom-2.mp3");
+        case 'w':
+            let tom2Sound = new Audio('sounds/tom-2.mp3');
             tom2Sound.play();
         break;
 
-        case "e":
-            let tom3Sound = new Audio("sounds/tom-3.mp3");
+        case 'e':
+            let tom3Sound = new Audio('sounds/tom-3.mp3');
             tom3Sound.play();
         break;
 
-        case "r":
-            let tom4Sound = new Audio("sounds/tom-4.mp3");
+        case 'r':
+            let tom4Sound = new Audio('sounds/tom-4.mp3');
             tom4Sound.play();
         break;
 
-        case "t":
-            let snareSound = new Audio("sounds/snare.mp3");
+        case 't':
+            let snareSound = new Audio('sounds/snare.mp3');
             snareSound.play();
         break;
 
-        case "y":
-            let crashSound = new Audio("sounds/crash.mp3");
+        case 'y':
+            let crashSound = new Audio('sounds/crash.mp3');
             crashSound.play();
         break;
 
-        case "u":
-            let kickBassSound = new Audio("sounds/kick-bass.mp3");
+        case 'u':
+            let kickBassSound = new Audio('sounds/kick-bass.mp3');
             kickBassSound.play();
         break;
 
         default:
             console.log("You've pressed wrong button - please choose a button exposed on the screen");
         break;
-    }
-
+    }   
 }
 
-    // Drum button pressing animation
-function drumButtonAnimation(currentKey) { // currentKey - a char that's assigned to the actually pressed drum button
-    let activeButton = document.querySelector("." + currentKey);
+/**
+ * currentKey - a char that's assigned to the actually pressed drum btn - it is located in the index.html
+ */
+let drumButtonAnimation = (currentKey) => {
+    let activeButton = document.querySelector('.' + currentKey);
 
-    activeButton.classList.add("pressed");
+    activeButton.classList.add('pressed');
 
     setTimeout(function() {
-        activeButton.classList.remove("pressed");
+        activeButton.classList.remove('pressed');
     }, 100) // 0.1s of pressing animation's duration
 }
+
+    // gets the current date
+let currentDate = new Date();
+
+    // gets the current year
+let currentYear = currentDate.getFullYear();
+
+    // shows the current year in the given tag
+document.querySelector('.p-curr-year').textContent = currentYear;
